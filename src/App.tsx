d222f65e686a0615c6454993fcd6363e9f4b5331
SHIVAM RAJPUT <132747669+shivamrajputa400@users.noreply.github.com>
2026-03-29 02:58:44 +0530

    Remove duplicate Skills component from App

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ function App() {
           <main>
             <Hero />
             <About />
+             <Skills />
             <Projects />
-            <Skills />
             <Contact />
           </main>
           <Footer />
